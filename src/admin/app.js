d9fbb608fdd23bde8873c3837c282c938a3b8622
Dnpdx <44032516+Dnpdx@users.noreply.github.com>
2023-02-02 13:57:10 -0600

    cambios 2

--- src/admin/app.js
+++ src/admin/app.js
@@ -1,64 +1,63 @@
-const config = {
-  locales: [
-    // 'ar',
-    // 'fr',
-    // 'cs',
-    // 'de',
-    // 'dk',
-    'es',
-    // 'he',
-    // 'id',
-    // 'it',
-    // 'ja',
-    // 'ko',
-    // 'ms',
-    // 'nl',
-    // 'no',
-    // 'pl',
-    // 'pt-BR',
-    // 'pt',
-    // 'ru',
-    // 'sk',
-    // 'sv',
-    // 'th',
-    // 'tr',
-    // 'uk',
-    // 'vi',
-    // 'zh-Hans',
-    // 'zh',
-  ],
-  theme: {
-    light: {
-      colors: {
-        primary100: '#D1007F',
-        primary200: '#D1007F',
-        primary500: '#ffffff',
-        buttonPrimary500: '#D1007F',
-        primary600: '#ffffff',
-        buttonPrimary600: '#D1007F',
-
-      }
-    },
-    dark: {
-      colors: {
-
-        primary100: '#D1007F',
-        primary200: '#D1007F',
-        primary500: '#ffffff',
-        buttonPrimary500: '#D1007F',
-        primary600: '#ffffff',
-        buttonPrimary600: '#D1007F',
-
-      }
-    }
-  }
-};
-
-const bootstrap = (app) => {
-  console.log(app);
-};
+import AuthLogo from './extensions/logoine.png';
+import MenuLogo from './extensions/logoine.png';
+import favicon from './extensions/favicon.ico';
 
 export default {
-  config,
-  bootstrap,
-};
+
+  config : {
+    auth: {
+      logo: AuthLogo,
+    },
+    menu: {
+      logo: MenuLogo,
+    },
+    head: {
+       favicon: favicon,
+    },
+    locales: [
+      'es',
+      // 'he',
+      // 'id',
+      // 'it',
+      // 'ja',
+      // 'ko',
+      // 'ms',
+      // 'nl',
+    ],
+    translations: {
+      es: {
+        "Auth.form.button.login.strapi": "Iniciar sesión",
+        "Auth.form.welcome.subtitle": "Inicie sesión en su cuenta",
+        "app.components.LeftMenu.navbrand.title": "Panel de control",
+      }
+    },
+    theme: {
+      dark: {
+        colors: {
+          primary100: '#D1007F',
+          primary200: '#D1007F',
+          primary500: '#ffffff',
+          buttonPrimary500: '#D1007F',
+          primary600: '#ffffff',
+          buttonPrimary600: '#D1007F',
+        }
+      },
+      light : {
+        colors: {
+          primary100: '#D1007F',
+          primary200: '#D1007F',
+          primary500: '#ffffff',
+          buttonPrimary500: '#D1007F',
+          primary600: '#ffffff',
+          buttonPrimary600: '#D1007F',
+        }
+      }
+      },
+     tutorials: false,
+     notifications: { release: false },
+    },
+
+    bootstrap() {},
+  }
+
+
